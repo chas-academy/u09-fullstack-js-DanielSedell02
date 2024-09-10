005f384244cb24bedf03eb27280558972902b7f1
DanielSedell02 <Danne.sedell@gmail.com>
2024-09-10 10:22:27 +0200

    Folder structur for frontend setup

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <h1 className="text-3xl font-bold underline text-center text-red-700">
       Hello world!
