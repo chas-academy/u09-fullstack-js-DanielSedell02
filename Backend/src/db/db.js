13aebc5110bbbb7ab31452f4bca9847a98915bde
DanielSedell02 <Danne.sedell@gmail.com>
2024-09-16 09:22:00 +0200

    quick change on db connection

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
 const MONGO_URI =
+  process.env.MONGO_URI ||
   "mongodb+srv://danielsedell:Daniel.02@cluster0.ei8vlsg.mongodb.net/";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
